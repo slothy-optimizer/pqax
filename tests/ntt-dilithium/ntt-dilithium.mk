0d65fc450fb6e74ec978c206e915a15e206eccca
willieyz <willie.zhao@chelpis.com>
2025-05-23 17:47:22 +0800

    refator: refactor the ntt-dilithium test
    Signed-off-by: willieyz <willie.zhao@chelpis.com>

--- tests/ntt-dilithium/ntt-dilithium.mk
+++ tests/ntt-dilithium/ntt-dilithium.mk
@@ -16,46 +16,60 @@ NTT_DILITHIUM_SOURCES += pqclean.c
 NTT_DILITHIUM_SOURCES += neonntt.c
 
 # Assembly sources required for this test
+
+
+NTT_DILITHIUM_SLOTHY_DIR = ../../slothy/
+NTT_DILITHIUM_SLOTHY_ASM_NAIVE = $(NTT_DILITHIUM_SLOTHY_DIR)/examples/naive/aarch64/dilithium
+NTT_DILITHIUM_SLOTHY_ASM_OPT = $(NTT_DILITHIUM_SLOTHY_DIR)/examples/opt/aarch64/dilithium
+NTT_DILITHIUM_SLOTHY_ASM_PAPER_OPT = $(NTT_DILITHIUM_SLOTHY_DIR)/paper/opt/neon
+NTT_DILITHIUM_SLOTHY_ASM_PAPER_CLEAN = $(NTT_DILITHIUM_SLOTHY_DIR)/paper/clean/neon
+
+
 NTT_DILITHIUM_ASM_DIR = ../../asm/manual/ntt_dilithium
 NTT_DILITHIUM_ASMS += pqclean_asm.s
 NTT_DILITHIUM_ASMS += neonntt_asm.s
-NTT_DILITHIUM_ASMS += $(NTT_DILITHIUM_ASM_DIR)/intt_dilithium_123_45678_manual_ld4_opt_a55.s
-NTT_DILITHIUM_ASMS += $(NTT_DILITHIUM_ASM_DIR)/intt_dilithium_123_45678_manual_ld4_opt_a72.s
-NTT_DILITHIUM_ASMS += $(NTT_DILITHIUM_ASM_DIR)/intt_dilithium_123_45678_manual_ld4_opt_m1_firestorm.s
-NTT_DILITHIUM_ASMS += $(NTT_DILITHIUM_ASM_DIR)/intt_dilithium_123_45678_manual_ld4_opt_m1_icestorm.s
-NTT_DILITHIUM_ASMS += $(NTT_DILITHIUM_ASM_DIR)/intt_dilithium_123_45678_manual_ld4.s
-NTT_DILITHIUM_ASMS += $(NTT_DILITHIUM_ASM_DIR)/intt_dilithium_123_45678_opt_a55.s
-NTT_DILITHIUM_ASMS += $(NTT_DILITHIUM_ASM_DIR)/intt_dilithium_123_45678_opt_a72.s
-NTT_DILITHIUM_ASMS += $(NTT_DILITHIUM_ASM_DIR)/intt_dilithium_123_45678_opt_m1_firestorm.s
-NTT_DILITHIUM_ASMS += $(NTT_DILITHIUM_ASM_DIR)/intt_dilithium_123_45678_opt_m1_icestorm.s
-NTT_DILITHIUM_ASMS += $(NTT_DILITHIUM_ASM_DIR)/intt_dilithium_123_45678.s
-NTT_DILITHIUM_ASMS += $(NTT_DILITHIUM_ASM_DIR)/intt_dilithium_1234_5678_manual_ld4_opt_a55.s
-NTT_DILITHIUM_ASMS += $(NTT_DILITHIUM_ASM_DIR)/intt_dilithium_1234_5678_manual_ld4_opt_a72.s
-NTT_DILITHIUM_ASMS += $(NTT_DILITHIUM_ASM_DIR)/intt_dilithium_1234_5678_manual_ld4_opt_m1_firestorm.s
-NTT_DILITHIUM_ASMS += $(NTT_DILITHIUM_ASM_DIR)/intt_dilithium_1234_5678_manual_ld4_opt_m1_icestorm.s
-NTT_DILITHIUM_ASMS += $(NTT_DILITHIUM_ASM_DIR)/intt_dilithium_1234_5678_manual_ld4.s
-NTT_DILITHIUM_ASMS += $(NTT_DILITHIUM_ASM_DIR)/intt_dilithium_1234_5678_opt_a55.s
-NTT_DILITHIUM_ASMS += $(NTT_DILITHIUM_ASM_DIR)/intt_dilithium_1234_5678_opt_a72.s
-NTT_DILITHIUM_ASMS += $(NTT_DILITHIUM_ASM_DIR)/intt_dilithium_1234_5678_opt_m1_firestorm.s
-NTT_DILITHIUM_ASMS += $(NTT_DILITHIUM_ASM_DIR)/intt_dilithium_1234_5678_opt_m1_icestorm.s
-NTT_DILITHIUM_ASMS += $(NTT_DILITHIUM_ASM_DIR)/intt_dilithium_1234_5678.s
-NTT_DILITHIUM_ASMS += $(NTT_DILITHIUM_ASM_DIR)/ntt_dilithium_123_45678_manual_st4_opt_a55.s
-NTT_DILITHIUM_ASMS += $(NTT_DILITHIUM_ASM_DIR)/ntt_dilithium_123_45678_manual_st4_opt_a72.s
-NTT_DILITHIUM_ASMS += $(NTT_DILITHIUM_ASM_DIR)/ntt_dilithium_123_45678_manual_st4_opt_m1_firestorm.s
-NTT_DILITHIUM_ASMS += $(NTT_DILITHIUM_ASM_DIR)/ntt_dilithium_123_45678_manual_st4_opt_m1_icestorm.s
-NTT_DILITHIUM_ASMS += $(NTT_DILITHIUM_ASM_DIR)/ntt_dilithium_123_45678_manual_st4.s
-NTT_DILITHIUM_ASMS += $(NTT_DILITHIUM_ASM_DIR)/ntt_dilithium_123_45678_opt_a55.s
-NTT_DILITHIUM_ASMS += $(NTT_DILITHIUM_ASM_DIR)/ntt_dilithium_123_45678_opt_a72.s
-NTT_DILITHIUM_ASMS += $(NTT_DILITHIUM_ASM_DIR)/ntt_dilithium_123_45678_opt_m1_firestorm.s
-NTT_DILITHIUM_ASMS += $(NTT_DILITHIUM_ASM_DIR)/ntt_dilithium_123_45678_opt_m1_icestorm.s
-NTT_DILITHIUM_ASMS += $(NTT_DILITHIUM_ASM_DIR)/ntt_dilithium_123_45678_w_scalar_opt_a55.s
-NTT_DILITHIUM_ASMS += $(NTT_DILITHIUM_ASM_DIR)/ntt_dilithium_123_45678_w_scalar_opt_m1_icestorm.s
-NTT_DILITHIUM_ASMS += $(NTT_DILITHIUM_ASM_DIR)/ntt_dilithium_123_45678_w_scalar.s
-NTT_DILITHIUM_ASMS += $(NTT_DILITHIUM_ASM_DIR)/ntt_dilithium_123_45678.s
-NTT_DILITHIUM_ASMS += $(NTT_DILITHIUM_ASM_DIR)/ntt_dilithium_1234_5678_manual_st4_opt_m1_firestorm.s
-NTT_DILITHIUM_ASMS += $(NTT_DILITHIUM_ASM_DIR)/ntt_dilithium_1234_5678_manual_st4_opt_m1_icestorm.s
-NTT_DILITHIUM_ASMS += $(NTT_DILITHIUM_ASM_DIR)/ntt_dilithium_1234_5678_manual_st4.s
-NTT_DILITHIUM_ASMS += $(NTT_DILITHIUM_ASM_DIR)/ntt_dilithium_1234_5678_opt_a72.s
-NTT_DILITHIUM_ASMS += $(NTT_DILITHIUM_ASM_DIR)/ntt_dilithium_1234_5678_opt_m1_firestorm.s
-NTT_DILITHIUM_ASMS += $(NTT_DILITHIUM_ASM_DIR)/ntt_dilithium_1234_5678_opt_m1_icestorm.s
-NTT_DILITHIUM_ASMS += $(NTT_DILITHIUM_ASM_DIR)/ntt_dilithium_1234_5678.s
+
+NTT_DILITHIUM_ASMS += $(NTT_DILITHIUM_SLOTHY_ASM_PAPER_CLEAN)/ntt_dilithium_1234_5678.s
+NTT_DILITHIUM_ASMS += $(NTT_DILITHIUM_SLOTHY_ASM_NAIVE)/intt_dilithium_1234_5678.s
+NTT_DILITHIUM_ASMS += $(NTT_DILITHIUM_SLOTHY_ASM_NAIVE)/ntt_dilithium_1234_5678_manual_st4.s
+NTT_DILITHIUM_ASMS += $(NTT_DILITHIUM_SLOTHY_ASM_OPT)/ntt_dilithium_1234_5678_manual_st4_opt_m1_firestorm.s
+NTT_DILITHIUM_ASMS += $(NTT_DILITHIUM_SLOTHY_ASM_OPT)/ntt_dilithium_1234_5678_manual_st4_opt_m1_icestorm.s
+NTT_DILITHIUM_ASMS += $(NTT_DILITHIUM_SLOTHY_ASM_PAPER_CLEAN)/ntt_dilithium_123_45678_manual_st4.s
+NTT_DILITHIUM_ASMS += $(NTT_DILITHIUM_SLOTHY_ASM_PAPER_OPT)/ntt_dilithium_123_45678_manual_st4_opt_a55.s
+NTT_DILITHIUM_ASMS += $(NTT_DILITHIUM_SLOTHY_ASM_PAPER_OPT)/ntt_dilithium_123_45678_manual_st4_opt_a72.s
+NTT_DILITHIUM_ASMS += $(NTT_DILITHIUM_SLOTHY_ASM_OPT)/ntt_dilithium_123_45678_manual_st4_opt_m1_firestorm.s
+NTT_DILITHIUM_ASMS += $(NTT_DILITHIUM_SLOTHY_ASM_OPT)/ntt_dilithium_123_45678_manual_st4_opt_m1_icestorm.s
+NTT_DILITHIUM_ASMS += $(NTT_DILITHIUM_SLOTHY_ASM_PAPER_CLEAN)/ntt_dilithium_123_45678.s
+NTT_DILITHIUM_ASMS += $(NTT_DILITHIUM_SLOTHY_ASM_NAIVE)/intt_dilithium_123_45678.s
+NTT_DILITHIUM_ASMS += $(NTT_DILITHIUM_SLOTHY_ASM_PAPER_CLEAN)/ntt_dilithium_123_45678_w_scalar.s
+NTT_DILITHIUM_ASMS += $(NTT_DILITHIUM_SLOTHY_ASM_PAPER_OPT)/ntt_dilithium_123_45678_w_scalar_opt_a55.s
+NTT_DILITHIUM_ASMS += $(NTT_DILITHIUM_SLOTHY_ASM_OPT)/ntt_dilithium_123_45678_w_scalar_opt_m1_icestorm.s
+NTT_DILITHIUM_ASMS += $(NTT_DILITHIUM_SLOTHY_ASM_NAIVE)/intt_dilithium_1234_5678_manual_ld4.s
+NTT_DILITHIUM_ASMS += $(NTT_DILITHIUM_SLOTHY_ASM_OPT)/intt_dilithium_1234_5678_manual_ld4_opt_a55.s
+NTT_DILITHIUM_ASMS += $(NTT_DILITHIUM_SLOTHY_ASM_OPT)/intt_dilithium_1234_5678_manual_ld4_opt_a72.s
+NTT_DILITHIUM_ASMS += $(NTT_DILITHIUM_SLOTHY_ASM_OPT)/intt_dilithium_1234_5678_manual_ld4_opt_m1_firestorm.s
+NTT_DILITHIUM_ASMS += $(NTT_DILITHIUM_SLOTHY_ASM_OPT)/intt_dilithium_1234_5678_manual_ld4_opt_m1_icestorm.s
+NTT_DILITHIUM_ASMS += $(NTT_DILITHIUM_SLOTHY_ASM_NAIVE)/intt_dilithium_123_45678_manual_ld4.s
+NTT_DILITHIUM_ASMS += $(NTT_DILITHIUM_SLOTHY_ASM_OPT)/intt_dilithium_123_45678_manual_ld4_opt_a55.s
+NTT_DILITHIUM_ASMS += $(NTT_DILITHIUM_SLOTHY_ASM_OPT)/intt_dilithium_123_45678_manual_ld4_opt_a72.s
+NTT_DILITHIUM_ASMS += $(NTT_DILITHIUM_SLOTHY_ASM_OPT)/intt_dilithium_123_45678_manual_ld4_opt_m1_firestorm.s
+NTT_DILITHIUM_ASMS += $(NTT_DILITHIUM_SLOTHY_ASM_OPT)/intt_dilithium_123_45678_manual_ld4_opt_m1_icestorm.s
+NTT_DILITHIUM_ASMS += $(NTT_DILITHIUM_SLOTHY_ASM_OPT)/intt_dilithium_123_45678_opt_a55.s
+NTT_DILITHIUM_ASMS += $(NTT_DILITHIUM_SLOTHY_ASM_OPT)/intt_dilithium_123_45678_opt_a72.s
+NTT_DILITHIUM_ASMS += $(NTT_DILITHIUM_SLOTHY_ASM_OPT)/intt_dilithium_123_45678_opt_m1_firestorm.s
+NTT_DILITHIUM_ASMS += $(NTT_DILITHIUM_SLOTHY_ASM_OPT)/intt_dilithium_123_45678_opt_m1_icestorm.s
+NTT_DILITHIUM_ASMS += $(NTT_DILITHIUM_SLOTHY_ASM_OPT)/intt_dilithium_1234_5678_opt_a55.s
+NTT_DILITHIUM_ASMS += $(NTT_DILITHIUM_SLOTHY_ASM_OPT)/intt_dilithium_1234_5678_opt_a72.s
+NTT_DILITHIUM_ASMS += $(NTT_DILITHIUM_SLOTHY_ASM_PAPER_OPT)/ntt_dilithium_1234_5678_opt_a72.s
+NTT_DILITHIUM_ASMS += $(NTT_DILITHIUM_SLOTHY_ASM_OPT)/intt_dilithium_1234_5678_opt_m1_firestorm.s
+NTT_DILITHIUM_ASMS += $(NTT_DILITHIUM_SLOTHY_ASM_OPT)/ntt_dilithium_1234_5678_opt_m1_firestorm.s
+NTT_DILITHIUM_ASMS += $(NTT_DILITHIUM_SLOTHY_ASM_OPT)/intt_dilithium_1234_5678_opt_m1_icestorm.s
+NTT_DILITHIUM_ASMS += $(NTT_DILITHIUM_SLOTHY_ASM_PAPER_OPT)/ntt_dilithium_123_45678_opt_a55.s
+NTT_DILITHIUM_ASMS += $(NTT_DILITHIUM_SLOTHY_ASM_PAPER_OPT)/ntt_dilithium_123_45678_opt_a72.s
+NTT_DILITHIUM_ASMS += $(NTT_DILITHIUM_SLOTHY_ASM_OPT)/ntt_dilithium_123_45678_opt_m1_firestorm.s
+NTT_DILITHIUM_ASMS += $(NTT_DILITHIUM_SLOTHY_ASM_OPT)/ntt_dilithium_123_45678_opt_m1_icestorm.s
+NTT_DILITHIUM_ASMS += $(NTT_DILITHIUM_SLOTHY_ASM_OPT)/ntt_dilithium_1234_5678_opt_m1_icestorm.s
+
+
+
+
